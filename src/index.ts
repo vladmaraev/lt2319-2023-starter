ef3401417b9727c74d66dfb0afe051c731379bb2
Devix71 <nitronitro60@yahoo.com>
2023-09-13 21:19:05 +0200

    added entity extraction for slots

--- src/index.ts
+++ src/index.ts
@@ -35,10 +35,34 @@ const listen =
     context.spstRef.send({
       type: "LISTEN",
     });
+const extractEntities = (utterance) => {
+  const entities = {
+    person: null,
+    day: null,
+    time: null,
+  };
 
+  const words = utterance.split(' ');
+
+  words.forEach((word, index) => {
+    if (word === "with" && words[index + 1]) {
+      entities.person = words[index + 1];
+    }
+    if (["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].includes(word)) {
+      entities.day = word;
+    }
+    if (["noon", "morning", "evening", "night"].includes(word)) {
+      entities.time = word;
+    }
+  });
+
+  return entities;
+};
+    
 // machine
 const dmMachine = createMachine(
   {
+    /** @xstate-layout N4IgpgJg5mDOIC5QCcD2qAuA6AIgSwEMAbVKAVzAFkCA7AmZLABWTAAcDWBiAQQGUASgBUhfAPoCAojxwBNANoAGALqJQbVLDwY8qGmpAAPRACYAzIqwAOK4pPmA7AFYXVswE4zAGhABPRACMDmbWVp5mZk4AbAGKZgAsik4Avsk+aJi4hCTkVLT0YIwCYAQQvlgA4qxgOjRQXHxM0gDSYgDCAPKUTAAykkKSSqpIIBpaOnoGxggR8VgO8QsmiopR9vHxAU4+-ghR7g5Y7k6K8e7xZlG2a-Gp6ejY+MSkFNR0DFjFpeUAEqgA7m1aABJH5gIhsBpNHitTrdPoDIYGMbaXT6EbTfZzJwBCxXeK2Byndw7RD7Q7HYkXK6rEy3NIgDKPbIvPLvQqfEplLA8WAAay4Uk6FQAcsC+JIcEiRiiJujQNNgk4sAEovF7O5VVEoit4qSEE4TFEsEarAFNYp3CY3Is7oyHllnrk3gUilzysU2CUMFCWu0ur1+oMVMjNKjJhiyecsDi8VYCYoiWd9WqrCqzA53FYiREM3amZUAKrA5gFHqoUqQLhtHrAtrNaXqMNyqamA7zM32OzLMxbYL69yW6w4i4BWIBKxrKz5h0VYtYYF0ADGOgAbmBeIIROIpDIFCGZc20a2ENaQjrVXFrdqnGYTPqAuZ3FhIopzY+xx4nA4Z5k5yWeBXPB1wXCAiA3bcxD4IQeGESVG1GI8IwVRBexMeYrhMAJ1QuDMggfZw5mWXE1WCExKR-BkC3-HkgJA4EwI3fgBCgmC4KlA8m3GY9IxmR8MKsLCcIiBx8L8QJUyON8wgWaJbFVX9sBowC1zALA+C9Ag+TwOpfRhf14SDBDZR4lC+PQhxMOwukRLE3YglOF8bCsb8PEEpxpyo2d5xU4C1J6PBYAwMAaB0+ohQ6UVxXgzjEO45CjFQ28sE2K07ENTUDm8cSEC2dxn0UMIPDfexYk8VIGRoVAIDgAwmVDeL5UShAAFoTAcfU2rmFYet6vrKPuTInhyV58gYBrwya6YsJTEI73IsdbySKIRPpQbmSdUb2UYFh2E4MAJpbXiHHvHKtksNUPIiNwYl7R9FMdEa2VdTlvkO0zmv2fULjTc5by2RYwkNKIHuG1kXQ+L5uSqMAajC96EumMcOpyqIFnmVYs2WE67zHUGWWdMaOSh34ASBGhQXBNgEamxBb2VFbrQ2E6cRm1H0aJfZBMTcwsICfHNueyH3R5fkaZPQdlTc79PGpVUUd2aJ0OzQSJyiQ0PAG+0hoJraXpJzlNIwcXeKWrBGb+hbBPjfUXHQ6knGOCcsJidwBaeiHiZFhjwMkGgIBNszVTmGxNkSdx9nNM0FbJVUTSCdUM3iFbs35ry-2LQPmrZ3ZBPQlZ1dxfL4wjkwHpopgywrGqA8PRqTxOgchzvcJPHIuxbXTpT50XAg6IOuvJpPbMHxdlVVjRsItnVjZy58-us8xEkzpOOZEjWWJ42d8qu6LAD+9A8DF8COyJJWI5ImCBZTi7Nbte7-fVPUzTtLqY+DVxc3bxTwcbhch9yRYDsJOdWiZL7mDno-PyWAApBRCvDQeR0zKxi-pcMwYRaQEm2CvTU8wFoRGwsEA4FVkhAA */
     id: "root",
     type: "parallel",
     states: {
@@ -67,7 +91,7 @@ const dmMachine = createMachine(
                 on: { SPEAK_COMPLETE: "HowCanIHelp" },
               },
               HowCanIHelp: {
-                entry: say("You can say whatever you like."),
+                entry: say("Who would you like to have a meeting with?"),
                 on: { SPEAK_COMPLETE: "Ask" },
               },
               Ask: {
@@ -86,13 +110,39 @@ const dmMachine = createMachine(
               },
               Repeat: {
                 entry: ({ context }) => {
-                  context.spstRef.send({
-                    type: "SPEAK",
-                    value: { utterance: context.lastResult[0].utterance },
-                  });
+                  const entities = extractEntities(context.lastResult[0].utterance);
+                  
+                  // Update context with extracted entities
+                  context.person = entities.person || context.person;
+                  context.day = entities.day || context.day;
+                  context.time = entities.time || context.time;
+              
+                  // Decide what to prompt based on missing information
+                  if (!context.person) {
+                    context.spstRef.send({
+                      type: "SPEAK",
+                      value: { utterance: "Who are you meeting with?" },
+                    });
+                  } else if (!context.day) {
+                    context.spstRef.send({
+                      type: "SPEAK",
+                      value: { utterance: "On which day?" },
+                    });
+                  } else if (!context.time) {
+                    context.spstRef.send({
+                      type: "SPEAK",
+                      value: { utterance: "At what time?" },
+                    });
+                  } else {
+                    context.spstRef.send({
+                      type: "SPEAK",
+                      value: { utterance: "Meeting scheduled!" },
+                    });
+                  }
                 },
                 on: { SPEAK_COMPLETE: "Ask" },
               },
+              
               IdleEnd: {},
             },
           },
@@ -122,8 +172,10 @@ const dmMachine = createMachine(
           },
         },
       },
-    },
+    }, 
+    
   },
+  
   {
     // custom actions
     //
@@ -136,7 +188,7 @@ const dmMachine = createMachine(
       "speak.greeting": ({ context }) => {
         context.spstRef.send({
           type: "SPEAK",
-          value: { utterance: "Hello world!" },
+          value: { utterance: "Hello user!" },
         });
       },
       "speak.how-can-I-help": ({ context }) =>
