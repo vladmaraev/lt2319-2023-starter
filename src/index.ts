926bcc41deee4ffdc03157d967f2a2f761b77f30
Andrea <guscarrian@student.gu.se>
2023-10-13 12:17:05 +0200

    progress, i guess

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,8 @@ async function fetchFromChatGPT(prompt: string, max_tokens: number) {
   const myHeaders = new Headers();
   myHeaders.append(
     "Authorization",
-    "Bearer <it is a secret shhh>",
+    //"Bearer <it is a secret shhh>",
+    "Bearer ",
   );
   myHeaders.append("Content-Type", "application/json");
   const raw = JSON.stringify({
@@ -55,24 +56,24 @@ async function fetchFromChatGPT(prompt: string, max_tokens: number) {
 
 
 // Grammar
-const grammar = {
-  "the chemical element": {
-    entities: {
-      element_JSON: "The element is"
-      //element_JSON: "i like oranges"
-    },
-  },
-  "i want to know the atomic number": {
-    entities: {
-      atomicNumber_JSON: "The atomic number is"
-    },
-  },
-  "can you tell me the atomic number": {
-    entities: {
-      atomicNumber_JSON: "The atomic number is"
-    },
-  }
- }
+//const grammar = {
+//  "the chemical element": {
+//    entities: {
+//      element_entity: "The element is"
+//      //element_entity: "i like oranges"
+//    },
+//  },
+//  "i want to know the atomic number": {
+//    entities: {
+//      atomicNumber_entity: "The atomic number is"
+//    },
+//  },
+//  "can you tell me the atomic number": {
+//    entities: {
+//      atomicNumber_entity: "The atomic number is"
+//    },
+//  }
+// }
 
 
 // helper functions
@@ -92,17 +93,67 @@ const listen =
     });
 
 
-const checkEntityInGrammar = (entity: string, inputSentence: string) => {
-  const cleanedInput = inputSentence.toLowerCase().replace(/\?$/, '')
-  console.log('Input Sentence: ', cleanedInput)
-  if (cleanedInput in grammar) {
-    if (entity in grammar[cleanedInput].entities) {
-      console.log("grammar", grammar[cleanedInput].entities)
-      return grammar[cleanedInput].entities[entity];
+//const checkEntityInGrammar = (entity: string, inputSentence: string) => {
+//  const cleanedInput = inputSentence.toLowerCase().replace(/\?$/, '')
+//  console.log('Input Sentence: ', cleanedInput)
+//  if (cleanedInput in grammar) {
+//    if (entity in grammar[cleanedInput].entities) {
+//      console.log("GRAMMAR:", grammar[cleanedInput].entities[entity])
+//      console.log("TRUE")
+//      //return grammar[cleanedInput].entities[entity];
+//      return true;
+//  }
+//}
+//  console.log("FALSE")
+//  return false;
+//};
+
+const grammar = {
+  "the chemical element": {
+    entities: ["element_entity"]
+  },
+  "i want to know the atomic number": {
+    entities: ["atomicNumber_entity"]
+  },
+  "can you tell me the atomic number": {
+    entities: ["atomicNumber_entity"]
+  },
+  "can you tell me the atomic number and the atomic weight": {
+    entities: ["atomicNumber_entity", "atomicWeight_entity"]
   }
 }
-  return false;
+
+// works well with grammar above
+//const checkEntityInGrammar = (entity: string, inputSentence: string) => {
+//  const cleanedInput = inputSentence.toLowerCase().replace(/\?$/, '');
+//  console.log('Input Sentence: ', cleanedInput);
+//  if (cleanedInput in grammar) {
+//    if (grammar[cleanedInput].entities.includes(entity)) {
+//      console.log("GRAMMAR:", grammar[cleanedInput].entities);
+//      console.log("TRUE");
+//      return true;
+//    }
+//  }
+//  console.log("FALSE");
+//  return false;
+//};
+
+
+//just testing
+const checkEntityInGrammar = (inputSentence: string) => {
+  const cleanedInput = inputSentence.toLowerCase().replace(/\?$/, '');
+  console.log('Input Sentence: ', cleanedInput);
+  if (cleanedInput in grammar) {
+    console.log("GRAMMAR:", grammar[cleanedInput].entities);
+    console.log("TRUE");
+    return grammar[cleanedInput].entities;
+    //return true
+  }
+  console.log("FALSE");
+  return [];
 };
+
+
 
 
 // getting a random index for picking a random phrase
@@ -144,7 +195,7 @@ const dmMachine = createMachine(
             initial: "HowCanIHelp",
             states: {
               HowCanIHelp: {
-                entry: say("Hi there, buddy! How can I help you?"),
+                entry: say("Hi there, amigo! How can I help you?"),
                 on: { SPEAK_COMPLETE: "Ask" },
               },
               Ask: {
@@ -164,7 +215,7 @@ const dmMachine = createMachine(
               AskChatGPT: {
                 invoke: {
                   src: fromPromise(async({ input }) => {
-                    const gptData = await fetchFromChatGPT(input.lastResult[0].utterance + "The information should be in JSON format, including the following entities: element_JSON (the name of the chemical element) and atomicNumber_JSON.", 250);
+                    const gptData = await fetchFromChatGPT(input.lastResult[0].utterance + "The information should be in JSON format, including the following entities: element_JSON (the name of the chemical element), atomicNumber_JSON and atomicWeight_JSON.", 250);
                     return gptData;
                   }),
                   input: ({ context, event }) => ({
@@ -177,6 +228,7 @@ const dmMachine = createMachine(
                       assign({
                         element_JSON: ({ event }) => JSON.parse(event.output).element_JSON,
                         atomicNumber_JSON: ({ event }) => JSON.parse(event.output).atomicNumber_JSON,
+                        atomicWeight_JSON: ({ event }) => JSON.parse(event.output).atomicWeight_JSON,
                       }),
                     ],
                   },
@@ -194,31 +246,130 @@ const dmMachine = createMachine(
               Information: {
                 entry: listen(),
                 on: {
-                  RECOGNISED: [{
-                    target: "element",
-                    guard: ({ event }) => {
-                      console.log('GUARD ELEMENT');
-                      return checkEntityInGrammar("element_JSON", event.value[0].utterance);
+                  RECOGNISED: [
+                    {
+                      target: "checkEntities",
+                      actions: assign({
+                        entities: ({ event }) => {
+                          console.log("CHECK ENTITIES ACTIONS");
+                          console.log(checkEntityInGrammar(event.value[0].utterance));
+                          return checkEntityInGrammar(event.value[0].utterance);
+                          //checkEntityInGrammar(event.value[0].utterance);
+                        },
+                      }),
                     },
-                    actions: assign ({
-                      element: ({ event }) => checkEntityInGrammar("element_JSON", event.value[0].utterance),
-                    }),
-                  },
-                  {
-                    target: "atomicNumber",
-                    guard: ({ event }) => {
-                      console.log('GUARD ATOMIC NUMBER');
-                      return checkEntityInGrammar("atomicNumber_JSON", event.value[0].utterance);
-                    },
-                    actions: assign ({
-                      atomicNumber: ({ event }) => {
-                        console.log("ACTIONS ATOMIC NUMBER");
-                        return checkEntityInGrammar("atomicNumber_JSON", event.value[0].utterance);
+                  //  target: "element",
+                  //  guard: ({ event }) => {
+                  //    console.log('GUARD ELEMENT');
+                  //    return checkEntityInGrammar("element_entity", event.value[0].utterance);
+                  //  },
+                  //  actions: assign ({
+                  //    element: ({ event }) => checkEntityInGrammar("element_entity", event.value[0].utterance),
+                  //  }),
+                  //},
+                  //the guard and actions for each entity are now separate, and the checkEntityInGrammar function (latest version)
+                  //is called for each entity (no entity: string). 
+                  //The reason is that this allows me to handle each entity separately and 
+                  //to handle cases where multiple entities are present in the same input sentence
+                    {
+                      target: "element",
+                      guard: ({ event }) => {
+                        console.log('GUARD ELEMENT');
+                        const entities = checkEntityInGrammar(event.value[0].utterance);
+                        console.log('RETURN: ', entities.includes("element_entity"));
+                        return entities.includes("element_entity");
                       },
-                    }),
-                  },
+                      actions: assign ({
+                        element: ({ event }) => {
+                          console.log("ACTIONS ELEMENT");
+                          console.log('RETURN: ', checkEntityInGrammar(event.value[0].utterance).includes("element_entity"));
+                          return checkEntityInGrammar(event.value[0].utterance).includes("element_entity");
+                        },
+                      }),
+                    },
+                    {
+                      target: "atomicNumber",
+                      guard: ({ event }) => {
+                        console.log('GUARD ATOMIC NUMBER');
+                        const entities = checkEntityInGrammar(event.value[0].utterance);
+                        console.log('RETURN: ', entities.includes("atomicNumber_entity"));
+                        return entities.includes("atomicNumber_entity");
+                      },
+                      actions: assign({
+                        atomicNumber: ({ event }) => {
+                          console.log("ACTIONS ATOMIC NUMBER");
+                          console.log('RETURN: ', checkEntityInGrammar(event.value[0].utterance).includes("atomicNumber_entity"));
+                          return checkEntityInGrammar(event.value[0].utterance).includes("atomicNumber_entity");
+                        },
+                      }),
+                    },
+                    {
+                      target: "atomicWeight",
+                      guard: ({ event }) => {
+                        console.log('GUARD ATOMIC WEIGHT');
+                        const entities = checkEntityInGrammar(event.value[0].utterance);
+                        console.log('RETURN: ', entities.includes("atomicWeight_entity"));
+                        return entities.includes("atomicWeight_entity");
+                      },
+                      actions: assign({
+                        atomicWeight: ({ event }) => {
+                          console.log("ACTIONS ATOMIC WEIGHT");
+                          console.log('RETURN: ', checkEntityInGrammar(event.value[0].utterance).includes("atomicWeight_entity"));
+                          return checkEntityInGrammar(event.value[0].utterance).includes("atomicWeight_entity");
+                        },
+                      }),
+                    },
+                  //{
+                  //  target: "atomicNumber",
+                  //  guard: ({ event }) => {
+                  //    console.log('GUARD ATOMIC NUMBER');
+                  //    return checkEntityInGrammar("atomicNumber_entity", event.value[0].utterance);
+                  //  },
+                  //  actions: assign ({
+                  //    atomicNumber: ({ event }) => {
+                  //      console.log("ACTIONS ATOMIC NUMBER");
+                  //      return checkEntityInGrammar("atomicNumber_entity", event.value[0].utterance);
+                  //    },
+                  //  }),
+                  //},
+                  //{
+                  //  target: "atomicWeight",
+                  //  guard: ({ event }) => {
+                  //    console.log('GUARD ATOMIC WEIGHT');
+                  //    return checkEntityInGrammar("atomicWeight_entity", event.value[0].utterance);
+                  //  },
+                  //  actions: assign ({
+                  //    atomicWeight: ({ event }) => {
+                  //      console.log("ACTIONS ATOMIC WEIGHT");
+                  //      return checkEntityInGrammar("atomicWeight_entity", event.value[0].utterance);
+                  //    },
+                  //  }),
+                  //},
                 ],
                 },
+              },
+              checkEntities: {
+                  always: [
+                    {
+                      // should I have guards here?
+                      target: "atomicNumber",
+                      cond: ({ entities }) => {
+                        console.log('CHECKING ATOMIC NUMBER')
+                        entities.includes("atomicNumber_entity");
+                      }
+                    },
+                    {
+                      target: "atomicWeight",
+                      cond: ({ entities }) => entities.includes("atomicWeight_entity"),
+                    },
+                    {
+                      target: "atomicNumberAndWeight",
+                      cond: ({ entities }) =>
+                        entities.includes("atomicNumber_entity") &&
+                        entities.includes("atomicWeight_entity"),
+                    },
+                    // handling here other entities maybe
+                  ],
               },
               element: {
                 entry: ({ context}) => { 
@@ -229,8 +380,20 @@ const dmMachine = createMachine(
                 },
                 on: { SPEAK_COMPLETE: "ByeBye" },
               },
+              atomicNumberAndWeight: {
+                entry: ({ context }) => {
+                  console.log('IN ATOMIC NUMBER AND WEIGHT SPEAK')
+                  const utterance = `${getRandomPhrase()}, the atomic number is ${context.atomicNumber_JSON} and the atomic weight is ${context.atomicWeight_JSON}`;
+                  context.spstRef.send({
+                    type: "SPEAK",
+                    value: { utterance }
+                  });
+                },
+                on: { SPEAK_COMPLETE: "ByeBye" },
+              },
               atomicNumber: {
-                entry: ({ context}) => { 
+                entry: ({ context}) => {
+                  console.log('IN ATOMIC NUMBER SPEAK')
                   context.spstRef.send({ 
                     type: "SPEAK", 
                     value:{ utterance: `${getRandomPhrase()}, the atomic number is ${context.atomicNumber_JSON}`}
@@ -238,6 +401,42 @@ const dmMachine = createMachine(
                 },
                 on: { SPEAK_COMPLETE: "ByeBye" },
               },
+              atomicWeight: {
+                entry: ({ context}) => { 
+                  console.log('IN ATOMIC WEIGHT SPEAK')
+                  context.spstRef.send({ 
+                    type: "SPEAK", 
+                    value:{ utterance: `${getRandomPhrase()}, the atomic weight is ${context.atomicWeight_JSON}`}
+                  });
+                },
+                on: { SPEAK_COMPLETE: "ByeBye" },
+              },           
+              //atomicNumber: {
+              //  entry: ({ context }) => {
+              //    console.log('IN ATOMIC NUMBER SPEAK')
+              //    const utterance = context.atomicWeight
+              //      ? `${getRandomPhrase()}, the atomic number is ${context.atomicNumber_JSON} and the atomic weight is ${context.atomicWeight_JSON}`
+              //      : `${getRandomPhrase()}, the atomic number is ${context.atomicNumber_JSON}`;
+              //    context.spstRef.send({
+              //      type: "SPEAK",
+              //      value: { utterance }
+              //    });
+              //  },
+              //  on: { SPEAK_COMPLETE: "ByeBye" },
+              //},
+              //atomicWeight: {
+              //  entry: ({ context }) => {
+              //    console.log('IN ATOMIC WEIGHT SPEAK')
+              //    const utterance = context.atomicNumber
+              //      ? `${getRandomPhrase()}, the atomic number is ${context.atomicNumber_JSON} and the atomic weight is ${context.atomicWeight_JSON}`
+              //      : `${getRandomPhrase()}, the atomic weight is ${context.atomicWeight_JSON}`;
+              //    context.spstRef.send({
+              //      type: "SPEAK",
+              //      value: { utterance }
+              //    });
+              //  },
+              //  on: { SPEAK_COMPLETE: "ByeBye" },
+              //},
               ByeBye: {
                 entry: ({ context }) => {
                   context.spstRef.send({
