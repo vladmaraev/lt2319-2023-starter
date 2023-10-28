5a5a4098d928f366841166a3398ed4e22619c6fa
Stas Hakkarainen <120642749+aspends@users.noreply.github.com>
2023-10-28 20:47:18 +0200

    Update keys.ts

--- src/keys.ts
+++ src/keys.ts
@@ -5,5 +5,5 @@ export const azureCredentials = {
   };
 
 
-export const spoty = 'BQD9-_m5QVJz_rl4064fvhuT5_FlegPoQeJuf7kd4ILISfRGRbU3dpv0x18GbCmEK_ljIFA5F-7L02-sfgMjzDrDppiH2DG9GyL3wYwilyJ6pN4Gbjxo4hNX3w1Ph0dNwcndkXIqPNSc6SF3Xou4go7xJi_GfkGdwbiVaC6FcMrEIgj6Qeq6PNSiqIcEenjOIY5wgunFH16VMXvV5amMF4wkA_knIpNbcMd5oMkPnhw9BnykITgRMb-v5y5Qz2baX3zzldvLp6mS';
-export const device_id = '0d1841b0976bae2a3a310dd74c0f3df354899bc8';
+export const spoty = '';
+export const device_id = '';
